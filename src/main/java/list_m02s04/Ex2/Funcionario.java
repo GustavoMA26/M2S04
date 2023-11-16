ceb37367ae578f9e8674889c962200e065548ad5
Gustavo <gmalmeida26@gmail.com>
2023-11-15 21:41:21 -0300

    Ex 02 - Sobrescrita

---
+++ src/main/java/list_m02s04/Ex2/Funcionario.java
@@ -0,0 +1,8 @@
+package list_m02s04.Ex2;
+
+public class Funcionario {
+
+    public String obterCargo() {
+        return "Sou funcionário";
+    }
+}
---
+++ src/main/java/list_m02s04/Ex2/Gerente.java
@@ -0,0 +1,9 @@
+package list_m02s04.Ex2;
+
+public final class Gerente extends Funcionario{
+
+    @Override
+    public String obterCargo() {
+        return "Sou gerente";
+    }
+}
