0fcc2b323f1d2749dc815f0ae636ccab5acb0cc5
Gustavo <gmalmeida26@gmail.com>
2023-11-15 21:44:21 -0300

    Ex 05 - Super-Herois 1 - Cria Main

---
+++ src/main/java/list_m02s04/SuperHerois/Main.java
@@ -0,0 +1,10 @@
+package list_m02s04.SuperHerois;
+
+public class Main {
+    public static void main(String[] args) {
+        Aplicacao aplicacao = new Aplicacao();
+        aplicacao.executar();
+
+
+    }
+}
