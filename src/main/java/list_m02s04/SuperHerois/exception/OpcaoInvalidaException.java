b3fecf0264e53db30dbd5b48d2698c854e7c205e
Gustavo <gmalmeida26@gmail.com>
2023-11-15 21:50:49 -0300

    Ex 09 - Super-Herois 5 - Cria Tratamento de Exceção

---
+++ src/main/java/list_m02s04/SuperHerois/exception/OpcaoInvalidaException.java
@@ -0,0 +1,8 @@
+package list_m02s04.SuperHerois.exception;
+
+public class OpcaoInvalidaException extends Exception {
+
+    public OpcaoInvalidaException(String msg) {
+        super(msg);
+    }
+}
